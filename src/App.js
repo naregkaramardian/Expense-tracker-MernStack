c527cad0df26ebe81c8001be9b823a3d3a4329b4
Nareg Karamardian <nareg.Karamardian@gmail.com>
2020-03-08 13:31:42 -0400

    Created the basic UI for The expense tracker react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  {Header} from './components/header/header.component.jsx'
+import {Balance} from './components/balance/balance.component.jsx'
+import {IncomeExpenses} from './components/incomeExpenses/incomeExpenses.component'
+import {TransactionList}  from './components/transactionList/transactionList.components'
+import {AddTransaction} from './components/addTransaction/addTransaction.components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header />
+      <div className="container">
+          <Balance />
+          <IncomeExpenses /> 
+          <TransactionList />
+          <AddTransaction />
+      </div>
     </div>
   );
 }
