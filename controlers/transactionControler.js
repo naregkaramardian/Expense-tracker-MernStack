f5c752245b850c301a510dd3950cf41a6a2dca21
Nareg Karamardian <nareg.Karamardian@gmail.com>
2020-03-10 23:06:45 -0400

    added MongoDB database to the backend, added the routes, modified the controlers so that they connect with the database and can CRUD data

---
+++ controlers/transactionControler.js
@@ -0,0 +1,79 @@
+const Transaction = require('../models/Transaction')
+
+
+// @desc    Get Alll transactions
+// @route   Get /api/v1/transactions
+// @access  public
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find()
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: "Server Error"
+        })
+    }
+}
+
+// @desc    Add transactions
+// @route   POST /api/v1/transactions
+// @access  public
+exports.addTransactions = async (req, res, next) => {
+    try {
+        const {
+            text,
+            amount
+        } = req.body
+        const transaction = await Transaction.create(req.body)
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        })
+    } catch (err) {
+        if (err.name === "ValidationError") {
+            const messages = Object.values(err.errors).map(val => val.message)
+            return res.status(400).json({
+                success: false,
+                error: messages
+            })
+        } else {
+            return res.status(500).json({
+                success: false,
+                error: "Server Error"
+            })
+        }
+    }
+}
+
+
+// @desc    Delete transactions
+// @route   Get /api/v1/transactions/:id
+// @access  public
+exports.deleteTransactions = async (req, res, next) => {
+    try {
+        const transaction = await Transaction.findById(req.params.id)
+        if (!transaction) {
+            return res.status(404).json({
+                success: false,
+                error: "No transaction found"
+            })
+
+        } else {
+            await transaction.remove()
+            return res.status(200).json({
+                success: true,
+                data: {}
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: "Server Error"
+        })
+    }
+}
