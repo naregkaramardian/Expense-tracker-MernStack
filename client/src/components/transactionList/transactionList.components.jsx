7839872377499eaa7b45d3fac4573382de957f74
Nareg Karamardian <nareg.Karamardian@gmail.com>
2020-03-12 00:25:19 -0400

    modified the client side and connected to the MongoDb database so that the state can CRUD the data and it will be saved in the database

--- client/src/components/transactionList/transactionList.components.jsx
+++ client/src/components/transactionList/transactionList.components.jsx
@@ -1,19 +1,26 @@
-import React, { useContext } from "react"
+import React, { useContext , useEffect } from "react"
 import { GlobalContext } from "../../context/GlobalState"
 import {Transaction} from '../transaction/transaction'
 
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext)
+  const { transactions, getTransactions } = useContext(GlobalContext)
+
+  useEffect(() => {
+    getTransactions()
+  } , [])
 
   return (
     <div>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (
-          <Transaction key={transaction.id} transaction={transaction}/>
+        {transactions.map(transaction => ( 
+          <Transaction key={transaction._id} transaction={transaction}/>
+        
         ))}
       </ul>
     </div>
   )
 }
+
+
